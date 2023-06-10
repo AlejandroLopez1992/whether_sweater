e79e83c3a4ad212fe4c27fff3995283608455d6b
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-06-10 15:39:17 -0600

    Feat: Complete call to Mapquest API

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe MapquestService do
-  context "class methods" do
+  context "class methods", :vcr do
     it "location method returns latitude and longitude data" do
-      search = MapquestService.new.location("Denver,CO")
-      expect(search).to be_a hash
+      search = MapquestService.new("Denver,CO").location
+      expect(search).to be_a Hash
       expect(search[:results]).to be_an Array 
       search_data = search[:results].first 
 
       expect(search_data).to have_key :providedLocation
-      expect(search_data[:providedLocation]).to be_an Array 
-      expect(search_data[:providedLocation].first).to have_key :location
+      expect(search_data[:providedLocation]).to be_an Hash
+      expect(search_data[:providedLocation]).to have_key :location
 
       expect(search_data).to have_key :locations
       expect(search_data[:locations]).to be_an Array 
-      expect(search_data[:locations].first).to have_key :latlng
+      expect(search_data[:locations].first).to have_key :latLng
 
-      latlng = search_data[:locations].first
+      latlng = search_data[:locations].first[:latLng]
       expect(latlng).to be_an Hash 
       expect(latlng).to have_key :lat 
       expect(latlng).to have_key :lng
