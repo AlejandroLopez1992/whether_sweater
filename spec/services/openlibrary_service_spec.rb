4997b75c665c0f016ddd95a080a84db4f7376be4
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-06-12 09:55:21 -0600

    Feat: Complete service call to openlibrary API

--- spec/services/openlibrary_service_spec.rb
+++ spec/services/openlibrary_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe OpenlibraryService do
   context "instance methods", :vcr do
     it "returns book objects based on title input" do
-      search = OpenlibraryService.new("denver,co").search
+      search = OpenlibraryService.new("denver,co", 5).search
       expect(search).to be_a Hash
       expect(search).to have_key(:numFound)
       expect(search[:numFound]).to be_an Integer
@@ -16,7 +16,7 @@ RSpec.describe OpenlibraryService do
       books.each do |book|
         expect(book).to have_key(:title)
         expect(book[:title]).to be_an String
-
+        
         expect(book).to have_key(:isbn)
         expect(book[:isbn]).to be_an Array
 
